e691431aab23b3a1b366adcef0d1fd7e2c63c9b6
Raja178747 <mishrachanchal400@gmail.com>
2024-10-13 00:03:10 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.routes import items, clock_in
+import os
+import uvicorn
 
 app = FastAPI()
 
@@ -9,3 +11,7 @@ app.include_router(clock_in.router, prefix="/api/v2")
 @app.get("/")
 async def root():
     return {"message": "Welcome to the FastAPI CRUD application!"}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000)) 
+    uvicorn.run(app, host="0.0.0.0", port=port)
